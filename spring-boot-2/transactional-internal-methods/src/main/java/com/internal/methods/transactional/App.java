b7c2859ce238803a3c976a2c8fa931620dd7ab7a
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2019-11-27 09:33:53 +0530

    transaction annotation

--- spring-boot-2/transactional-internal-methods/src/main/java/com/internal/methods/transactional/App.java
+++ spring-boot-2/transactional-internal-methods/src/main/java/com/internal/methods/transactional/App.java
@@ -2,8 +2,10 @@ package com.internal.methods.transactional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
