de3d27d02a492d146ee9037917d72ca5837c5d3a
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2020-03-26 22:17:48 +0530

    spring boot

--- spring-boot-2/rest-api/src/main/java/com/rest/api/model/Student.java
+++ spring-boot-2/rest-api/src/main/java/com/rest/api/model/Student.java
@@ -1,12 +1,11 @@
 package com.rest.api.model;
 
+import java.util.Date;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 @Data
 @AllArgsConstructor
@@ -14,7 +13,8 @@ import java.util.Date;
 @Builder
 public class Student {
 
-    private String name;
-    private String cource;
-    private Date joiningDate;
+	private String id;
+	private String name;
+	private String cource;
+	private Date joiningDate;
 }
