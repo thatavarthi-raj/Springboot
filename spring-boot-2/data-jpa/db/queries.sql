b968c43e2f396d3080ec2092b6fd8b4e16075909
Avinash Babu Donthu <avinashbabu.donthu@gmail.com>
2020-09-13 16:08:04 +0530

    spring boot 13-09-2020 16:08:04.49

--- spring-boot-2/data-jpa/db/queries.sql
+++ spring-boot-2/data-jpa/db/queries.sql
@@ -4,3 +4,12 @@ show tables;
 -- emp
 select * from emp;
 select count(*) from emp;
+
+-- student
+select * from student;
+insert into student(name,course,joining_date) VALUES('jack', 'java', (select sysdate()));
+insert into student(name,course,joining_date) VALUES('jill', 'spring', (select sysdate()));
+insert into student(name,course,joining_date) VALUES('james', 'maven', (select sysdate()));
+delete from student;
+drop table student;
+commit;
